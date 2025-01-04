ea22bff04524cbed4473cb05ff663725c6fbea1f
quang huy <huyquang1282000@gmail.com>
2025-01-05 02:29:08 +0700

    change1

--- client/src/services/GetBaseUrl.jsx
+++ client/src/services/GetBaseUrl.jsx
@@ -2,14 +2,9 @@ function GetBaseUrl() {
     const hostname = window.location.hostname;
     if (hostname === 'localhost') {
         return 'http://localhost:3000';
-    } else if (hostname === '192.168.1.193') {
-        return 'http://192.168.1.193:3000';
-    } else if (hostname === '172.20.10.3') {
-        return 'http://172.20.10.3:3000';
-    } else if (hostname === '10.5.50.185') {
-        return 'http://10.5.50.185:3000';
+    } else {
+        return 'https://seff-furniure-server.vercel.app';
     }
-    return 'https://seff-furniure-server.vercel.app';
 }
 
 export default GetBaseUrl;
